0fe7f7290ce75bfab34a3519bb3cad53cafc660f
ForkadulIslam <forkadulislam@gmail.com>
2025-11-30 14:49:00 +0600

    SnowFall done

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,21 @@
   <div id="app">
     <!-- <SlotMachine /> -->
     <!-- <BurningSatellite /> -->
-    <!-- <CristmasReels /> -->
-    <DynamicParticles/>
+    <CristmasReels />
+    <!-- <DynamicParticles/> -->
+    <!-- <ShowFall/> -->
+
 
   </div>
 </template>
 
 <script setup>
 // import SlotMachine from './components/SlotMachine.vue';
-// import BurningSatellite from './components/BurningSatellite.vue';
-//import CristmasReels from './components/CristmasReels.vue';
+//import BurningSatellite from './components/BurningSatellite.vue';
+import CristmasReels from './components/CristmasReels.vue';
 //import CelebrationScreen from './components/CelebrationScreen.vue';
-import DynamicParticles from './components/DynamicParticles.vue';
+//import DynamicParticles from './components/DynamicParticles.vue';
+
+//import ShowFall from './components/ShowFall.vue';
 
 </script>
